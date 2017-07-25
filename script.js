619671f64242f364ae7df656b47689f7095477e6
Rebecca Wagaman <ribexy@gmail.com>
2017-07-25 22:56:46 +0000

    Breakpoint

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     var url =  "https://newsapi.org/v1/sources";
-    // var category = ;
     var data = {language: "en", country: "us"};
 
     $.ajax({
@@ -8,7 +7,23 @@ $(document).ready(function() {
         data: data,
         type: "GET",
         success: function(response){
-            console.log(response);
-        }
+            var sources = response.sources;
+            // console.log(sources);
+            var html = "<select class='form-control' id='source'>";
+            $.each(sources, function(index, source) {
+                // console.log(index, source.name);
+                html += "<option value='" + index + "'>"+ source.name +"</option>";
+            });
+            html += "</select>";
+            $(".form-group").html(html);
+            }
+        
+        
     });
+
+    
+
+    
+
 });
+
