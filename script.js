4cf82aab144231967c50946d650f463625602f6b
Rebecca Wagaman <ribexy@gmail.com>
2017-07-31 22:41:13 +0000

    Breakpoint again

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
         success: function(response){
             var sources = response.sources;
             // console.log(sources);
-            var html = "<select class='form-control' id='source'>";
+            var html = "<select class='form-control' id='sourceId'>";
             $.each(sources, function(index, source) {
                 // console.log(index, source.name);
-                html += "<option value='" + index + "'>"+ source.name +"</option>";
+                html += "<option value='" + source.id + "'>"+ source.name +"</option>";
             });
             html += "</select>";
             $(".form-group").html(html);
@@ -21,9 +21,29 @@ $(document).ready(function() {
         
     });
 
-    
+    $("#source").submit(function(event) {
+        event.preventDefault();
+        var id = $('#sourceId').val();
+        var url = "https://newsapi.org/v1/articles";
+        var data = {source: id, apiKey: "4264f6c6670848c3958a383f52213ef2", sortBy: "top"};
+        $.ajax({
+            url: url,
+            data: data,
+            type: "GET",
+            success: function(response) {
+                var articles = response.articles;
+                // console.log(articles);
+                var html = "<ul class='list-group'>";
+                $.each(articles, function(index, article) {
+                   html += "<li class='list-item'><a href=" + article.url + ">"+ article.title + "</a></li>"; 
+                });
+                html += "</ul>";
+                $("#articles").html(html);
+            }
+        })
+    });
 
-    
+    // link and description
 
 });
 
